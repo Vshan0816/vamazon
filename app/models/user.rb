1f1dd47054313c06bd9969610c9d84d734369459
Vincent <vshan0816@gmail.com>
2023-11-09 16:39:38 -0500

    finish user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,42 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, 
+    uniqueness: true, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
+
+  before_validation :ensure_session_token
+
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email)
+    if user&.authenticate(password)
+      return user
+    else 
+      return nil
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
+  def generate_unique_session_token
+    loop do 
+      token = SecureRandom.urlsafe_base64
+      return token if !User.exists?(session_token: token)
+    end
+  end
 end
