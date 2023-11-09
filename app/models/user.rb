ca024a2a241f6ec8df5920adf7cb05cc6961e9f0
Vincent <vshan0816@gmail.com>
2023-11-09 16:48:43 -0500

    fix self.find_by_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
 
   def self.find_by_credentials(email, password)
-    user = User.find_by(email)
+    user = User.find_by(email: email)
     if user&.authenticate(password)
       return user
     else 
