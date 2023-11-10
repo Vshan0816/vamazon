e87d79dc297558bc6772dda6063a8b038e7cd170
Vincent <vshan0816@gmail.com>
2023-11-10 16:23:58 -0500

    Working on styling login

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,7 +1,11 @@
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { login } from "../../store/session"
+import { ReactSVG } from "react-svg"
+import AmazonLogo from "../../assets/AmazonLogo.png"
+import './LoginForm.css'
+
 
 const LoginFormPage = () => {
     const dispatch = useDispatch()
@@ -13,15 +17,27 @@ const LoginFormPage = () => {
         dispatch(login({email, password}))
     }
     return (
+        
         <form onSubmit={handleSubmit}>
-            <label>Email
-                <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
-            </label>
-            <label>Password
-                <input type="text" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
-            </label>
-            <button>Sign In</button>
+            <div id="outerDiv">
+                <div id="innerDiv">
+                        <img src={AmazonLogo} alt="AmazonLogo"></img>
+                </div>
+            </div>   
+    
+            <div id="formDiv">      
+                    
+                    
+                    <label>Email
+                        <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                    </label>
+                    <label>Password
+                        <input type="text" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+                    </label>
+                    <button>Sign In</button>
+            </div>
         </form>
+        
     )
 }
 
