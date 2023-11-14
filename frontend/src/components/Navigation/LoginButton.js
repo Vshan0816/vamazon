79d0a5dcff0baa4232ee3575ef525c37ed8a381a
Vincent <vshan0816@gmail.com>
2023-11-14 13:42:28 -0500

    add dropdown

--- frontend/src/components/Navigation/LoginButton.js
+++ frontend/src/components/Navigation/LoginButton.js
@@ -1,15 +1,25 @@
 const LoginButton = ({user}) =>{
     if (user) {
        return (
-        <div className="loginDiv">
-            <p className="p1">hello, sign in</p>
-            <p className="p2">Account, & Lists</p>
-        </div>
+        <>  
+            <div></div>
+            <div className="loginDiv">
+                <span>
+                    <p className="p1">Hello, sign in</p>
+                    <p className="p2">Account, & Lists</p>
+                </span>
+                <div class="loginDiv-content">
+                    <button>Sign In</button>
+                </div>
+            </div>
+            <div></div>
+            <div className="cart"></div>
+        </>
        )
     } else {
         return
         <div className="loginDiv">
-            <p className="p1">hello, sign in</p>
+            <p className="p1">Hello, sign in</p>
             <p className="p2">Account, & Lists</p>
         </div>
     }
