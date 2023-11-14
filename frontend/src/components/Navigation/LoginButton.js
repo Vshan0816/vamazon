6b7dad67dddad8d3ed96b346ad78ea77fc9293c9
Vincent <vshan0816@gmail.com>
2023-11-14 14:48:36 -0500

    add logout function

--- frontend/src/components/Navigation/LoginButton.js
+++ frontend/src/components/Navigation/LoginButton.js
@@ -1,18 +1,29 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min"
+import { useDispatch } from "react-redux"
+import * as sessionActions from '../../store/session';
 
 const LoginButton = ({user}) =>{
+    const dispatch = useDispatch()
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+      };
+
     if (user) {
        return (
         <>  
             <div></div>
             <div className="loginDiv">
                 <span>
-                    <p className="p1">Hello, sign in</p>
+                    <p className="p1">Hello, {user.name}</p>
                     <p className="p2">Account, & Lists</p>
                 </span>
                 <div className="loginDiv-content">
-                    <Link to="/login"><button className="button">Sign In</button></Link>
-                    <p className="p3">New Customer? <Link to="/signup">Start Here</Link></p>
+                    <button
+                         className="button2"
+                         onClick={logout}
+                         >Sign Out
+                    </button>
                 </div>
             </div>
             <div></div>
@@ -20,11 +31,23 @@ const LoginButton = ({user}) =>{
         </>
        )
     } else {
-        return
-        <div className="loginDiv">
-            <p className="p1">Hello, sign in</p>
-            <p className="p2">Account, & Lists</p>
-        </div>
+        return (
+            <>  
+                <div></div>
+                <div className="loginDiv">
+                    <span>
+                        <p className="p1">Hello, sign in</p>
+                        <p className="p2">Account, & Lists</p>
+                    </span>
+                    <div className="loginDiv-content">
+                        <Link to="/login"><button className="button2">Sign In</button></Link>
+                        <p className="p3">New Customer? <Link to="/signup">Start Here</Link></p>
+                    </div>
+                </div>
+                <div></div>
+                <div className="cart"></div>
+            </>
+           )
     }
 }
 
