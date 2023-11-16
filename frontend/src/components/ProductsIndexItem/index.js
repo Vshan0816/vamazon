dd21e19102c3d6e45dacdf61cde6813015ff2ddb
Vincent <vshan0816@gmail.com>
2023-11-16 14:58:08 -0500

    styling product index page

--- frontend/src/components/ProductsIndexItem/index.js
+++ frontend/src/components/ProductsIndexItem/index.js
@@ -1,10 +1,15 @@
 import './ProductsIndexItem.css'
-
+import sf6 from '../../assets/SF6.png'
 const ProductsIndexItem = ({product}) => {
     return (
-        <div className="itemDiv">
-            <p>{product.name}</p>
-        </div>
+        <div className="outerImgDiv">  
+            <div className="itemImgDiv">
+                <img src={sf6}></img>
+            </div>
+            <div className="itemDiv">
+                <p>{product.name}</p>
+            </div>
+        </div>    
     )
 }
 
