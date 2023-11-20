459cf01c648bf3b2bb8f504042a6b984d3f98555
Vincent <vshan0816@gmail.com>
2023-11-20 11:39:31 -0500

    styling products show page

--- frontend/src/components/ProductsShow/index.js
+++ frontend/src/components/ProductsShow/index.js
@@ -3,6 +3,8 @@ import { fetchProduct, getProduct} from "../../store/product"
 import { useEffect } from "react"
 import { useSelector } from "react-redux/es/hooks/useSelector"
 import { useParams } from "react-router-dom/cjs/react-router-dom.min"
+import sf6 from '../../assets/SF6.png'
+import './ProductsShow.css'
 const ProductsShow = () => {
     const { productId } = useParams()
     const dispatch = useDispatch();
@@ -12,10 +14,20 @@ const ProductsShow = () => {
         dispatch(fetchProduct(productId))
     }, [productId, dispatch])
     return (
-        <ul>
-            <li>{product.name}</li>
-            <li>{product.description}</li>
-        </ul>
+        <div className="showOuterDiv">
+            
+            <div className="div1"></div>
+            <div className="showImageDiv">
+                <img src={sf6} alt="StreetFighter6" className="showImage" />
+            </div>
+            <div className="showBodyDiv">
+                
+                <p className="showName">{product.name}</p>
+                <div className="divider"></div>
+                <p className="showDescription">{product.description}</p>
+                
+            </div>
+        </div>    
     )
 }
 
