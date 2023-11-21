252085de0fa9c35cec16bba69473e79fea794f42
Vincent <vshan0816@gmail.com>
2023-11-21 11:20:49 -0500

    add cartItemsReducer to the store

--- frontend/src/store/cartItems.js
+++ frontend/src/store/cartItems.js
@@ -118,3 +118,5 @@ const cartItemsReducer = (state = {}, action) => {
             return state;
     }
 }
+
+export default cartItemsReducer
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import sessionReducer from './session';
 import productsReducer from './product';
+import cartItemsReducer from './cartItems';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
-  products: productsReducer
+  products: productsReducer,
+  cartItems: cartItemsReducer
 });
 
 let enhancer;
